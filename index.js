d0c0645ac22e697bd256e3097d1ac38c6d1d01d5
Oleksandr Khotemskyi <xotabu4@gmail.com>
2019-12-20 20:49:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const path = require("path");
 async function run() {
     try {
         console.log(`## STARTING`);
+        await exec.exec("pwd");
+        await exec.exec("ls");
+        await exec.exec("cd .. && ls");
         await exec.exec(path.join(__dirname, "index.sh"));
     } catch (error) {
         core.setFailed(error.message);
